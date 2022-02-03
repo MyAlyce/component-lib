a402106e53cca4557c4248bbb944db284320458d
Dovydas Stirpeika <Dovy91@gmail.com>
2022-02-03 15:44:03 -0600

    npm publish 0.0.12

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -8,7 +8,7 @@ export type SpinnerProps = {
     /** If not set or set to `"auto"` will automatically fill the available space */
     size?: Size | 'auto';
     type?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 'pulse';
-    className: string;
+    className?: string;
 }
 
 export const Spinner = ({ type = 1, size = 'auto', className }: SpinnerProps) => <div className={classNames("overflow-hidden", className, cssSizeMap[size])}>
